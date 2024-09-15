b3a2eb422d7ef5abe5802949f9d52def6cb20a34
Souvik Goswami <souvikgoswami464@gmail.com>
2024-09-15 09:46:11 +0000

    Data Type modified

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -5,7 +5,8 @@ let age = 22
 console.log(typeof(name))
 
 /*
-    premitive Data Types :
+    data types are based on how it stores in the memory and how we access it 
+    premitive Data Types : all the changes are made on the copy not in the actual memory 
         1. Number
         2. String
         3. Boolean
@@ -14,9 +15,10 @@ console.log(typeof(name))
         6. null
         7. symbol : unique (mostly used in React)
 
-    Non-premitive Data Types :
+    Non-premitive Data Types  / Reference Type :
         1. Object
         2. Arrays
+        3. Functions
 */
 
 console.log(typeof undefined)
@@ -25,3 +27,17 @@ console.log(typeof null)
 /*
     undefined is a type itself in javascript but null is type of object 
 */
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+console.log(id===anotherId)
+
+const colors = ["Red","Blue","Green"]       //  Arrays
+const user = {
+    name :"Souvik",
+    age:22,
+    city:"New Delhi"
+}
+function sayHi(){
+    console.log("Hi")
+}
