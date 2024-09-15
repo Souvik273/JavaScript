3e53d29422a387d26b8b4c61c733e4967172ba86
Souvik Goswami <souvikgoswami464@gmail.com>
2024-09-15 07:26:59 +0000

    Data types

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,27 @@
+const name = "Souvik"
+let city = "New Delhi"
+let age = 22
+
+console.log(typeof(name))
+
+/*
+    premitive Data Types :
+        1. Number
+        2. String
+        3. Boolean
+        4. bigint
+        5. undefined
+        6. null
+        7. symbol : unique (mostly used in React)
+
+    Non-premitive Data Types :
+        1. Object
+        2. Arrays
+*/
+
+console.log(typeof undefined)
+console.log(typeof null)
+
+/*
+    undefined is a type itself in javascript but null is type of object 
+*/
