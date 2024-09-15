8f89cb5ad21ea8d83a904a746e26213d97e05737
Souvik Goswami <souvikgoswami464@gmail.com>
2024-09-15 09:29:43 +0000

    Comparision Operartor

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+console.log("2">1)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+/*
+    The equity check == and the comparison <,>,<=,>= works differently 
+        The comparision converts null to a number that's why it will traet it as 0
+*/
+
+console.log("2"===2)
+
+/*
+    ===  not only check the value but also check the data types 
+*/
